7a6398a5e27c4b23e64d5b8950989b926c0a40a3
Furkan <Furkan.gundz@gmail.com>
2022-04-12 14:39:55 +0300

    Finished

--- src/components/GameComp.vue
+++ src/components/GameComp.vue
@@ -1,52 +1,55 @@
 <template>
-	<start-page v-if="!isGameStarted" />
-	<question-comp v-if="isGameStarted && !isGameFinished.finished" />
-	<finish-game @restart="reset" v-if="isGameStarted && isGameFinished.finished" />
+  <start-comp v-if="!isGameStarted" />
+  <question-comp v-if="isGameStarted && !isGameFinished.finished" />
+  <finish-game
+    @restart="reset"
+    v-if="isGameStarted && isGameFinished.finished"
+  />
 </template>
 <script>
-	import QuestionComp from '@/components/QuestionComp.vue';
-	import StartPage from '@/components/StartPage.vue';
-	import FinishGame from './FinishComp.vue';
-	import countries from '../../country_names';
+import QuestionComp from "@/components/QuestionComp.vue";
+import StartComp from "@/components/StartComp.vue";
+import FinishGame from "./FinishComp.vue";
+import countries from "../../country_names";
 
-	export default {
-		components: {
-			QuestionComp,
-			StartPage,
-			FinishGame,
-		},
-		data() {
-			return {
-				isGameStarted: false,
-				resetGame: { reset: false },
-				isGameFinished: { finished: false },
-				userName: { userName: '' },
-				countriesLen: countries.length,
-				score: { score: 0 },
-			};
-		},
-		methods: {
-			startGame(userName) {
-				if (!userName) {
-					alert('please write username');
-					return;
-				}
-				this.userName.userName = userName;
-				this.isGameStarted = true;
-			},
-			reset() {
-				this.isGameFinished.finished = false;
-				this.isGameStarted = false;
-			},
-		},
-		provide() {
-			return {
-				startGame: this.startGame,
-				isGameFinished: this.isGameFinished,
-				userName: this.userName,
-				countriesLen: this.countriesLen,
-				score: this.score,
-			};
-		},
-	};
+export default {
+  components: {
+    QuestionComp,
+    StartComp,
+    FinishGame,
+  },
+  data() {
+    return {
+      isGameStarted: false,
+      resetGame: { reset: false },
+      isGameFinished: { finished: false },
+      userName: { userName: "" },
+      countriesLen: countries.length,
+      score: { score: 0 },
+    };
+  },
+  methods: {
+    startGame(userName) {
+      if (!userName) {
+        alert("please write username");
+        return;
+      }
+      this.userName.userName = userName;
+      this.isGameStarted = true;
+    },
+    reset() {
+      this.isGameFinished.finished = false;
+      this.isGameStarted = false;
+    },
+  },
+  provide() {
+    return {
+      startGame: this.startGame,
+      isGameFinished: this.isGameFinished,
+      userName: this.userName,
+      countriesLen: this.countriesLen,
+      score: this.score,
+    };
+  },
+};
 </script>
--- src/components/QuestionComp.vue
+++ src/components/QuestionComp.vue
@@ -1,173 +1,174 @@
-<!-- @format -->
-
 <template>
-	<div class="card">
-		<div v-if="score.score > 0" class="score">
-			<p>
-				Score :
-				<span style="color: darkcyan; font-size: 1.5rem">{{ score.score }}</span>
-			</p>
+  <div class="card">
+    <div v-if="score.score > 0" class="score">
+      <p>
+        Score :
+        <span style="color: darkcyan; font-size: 1.5rem">{{
+          score.score
+        }}</span>
+      </p>
 
-			<p>
-				Left
-				<span style="color: darkcyan; font-size: 1.5rem">
-					{{ countriesLen - index + 1 }}</span
-				>
-				flag
-			</p>
-		</div>
-		<div class="flag">
-			<img :src="img" alt="" />
-		</div>
-		<div class="buttons">
-			<button @click="choose">{{ AnswersList[0] }}</button>
-			<button @click="choose">{{ AnswersList[1] }}</button>
-			<button @click="choose">{{ AnswersList[2] }}</button>
-			<button @click="choose">{{ AnswersList[3] }}</button>
-		</div>
-	</div>
+      <p>
+        Left
+        <span style="color: darkcyan; font-size: 1.5rem">
+          {{ countriesLen - index + 1 }}</span
+        >
+        flag
+      </p>
+    </div>
+    <div class="flag">
+      <img :src="img" alt="" />
+    </div>
+    <div class="buttons">
+      <button @click="choose">{{ AnswersList[0] }}</button>
+      <button @click="choose">{{ AnswersList[1] }}</button>
+      <button @click="choose">{{ AnswersList[2] }}</button>
+      <button @click="choose">{{ AnswersList[3] }}</button>
+    </div>
+  </div>
 </template>
 
 <script>
-	/*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
+/*eslint no-constant-condition: ["error", { "checkLoops": false }]*/
 
-	import countries from '../../country_names';
-	const axios = require('axios');
-	export default {
-		data() {
-			return {
-				db: 'https://my-json-server.typicode.com/Furkangunduz/flag-guesser-clone/leaderBoard',
-				country: '',
-				index: 0,
-				img: '',
-				AnswersList: ['', '', '', ''],
-				answer: '',
-				countriesLen: countries.length,
-			};
-		},
-		inject: ['isGameFinished', 'userName', 'score'],
+import countries from "../../country_names";
+const axios = require("axios");
+export default {
+  data() {
+    return {
+      db: "https://my-json-server.typicode.com/Furkangunduz/flag-guesser-clone/leaderBoard",
+      country: "",
+      index: 0,
+      img: "",
+      AnswersList: ["", "", "", ""],
+      answer: "",
+      countriesLen: countries.length,
+    };
+  },
+  inject: ["isGameFinished", "userName", "score"],
 
-		created() {
-			this.nextQuestion();
+  created() {
+    this.nextQuestion();
 
-			this.score.score = 0;
-		},
+    this.score.score = 0;
+  },
 
-		methods: {
-			//call This after every choice and check answer
-			choose(ClickedButton) {
-				if (ClickedButton) {
-					let buttons = document.querySelectorAll('button');
-					let btnText = ClickedButton.target.outerText;
-					//disabled buttons till next question
-					buttons.forEach((button) => {
-						button.disabled = true;
-					});
-					//if true make green button and wait for 1 sec
-					if (this.checkAnswer(btnText)) {
-						this.score.score++;
-						ClickedButton.target.classList.add('true');
-						setTimeout(() => {
-							if (this.index < countries.length) {
-								//enable buttons after 3 sec
-								buttons.forEach((button) => {
-									button.classList.remove('true');
-									button.classList.remove('false');
-									button.disabled = false;
-								});
-								this.nextQuestion();
-							}
-						}, 800);
-					}
-					//if not true make red button and wait 2 sec
-					else {
-						ClickedButton.target.classList.add('false');
-						buttons.forEach((button) => {
-							if (button.outerText == this.country) {
-								button.classList.add('true');
-							}
-						});
-						setTimeout(() => {
-							if (this.index <= countries.length) {
-								//enable buttons after 3 sec
-								buttons.forEach((button) => {
-									button.classList.remove('true');
-									button.classList.remove('false');
-									button.disabled = false;
-								});
-								this.nextQuestion();
-							}
-						}, 1000);
-					}
-				}
-				if (this.index == countries.length) {
-					this.isGameFinished.finished = true;
-					this.AddLeaderBoardUser();
-				}
-			},
-			//get next question and increase index of question
-			nextQuestion() {
-				this.getData();
-				this.index++;
-			},
-			// get data from api with axios then set variables for questions
-			getData() {
-				axios.get(`https://restcountries.com/v3.1/name/${countries[this.index]}`).then(
-					(res) => {
-						this.country = res.data[0].name.common;
-						this.img = res.data[0].flags['png'];
-						this.setAnswers();
-					}
-				);
-			},
-			//set 1 correct answer from question info and
-			//3 random answers from countries list
-			setAnswers() {
-				let correctIndex = Math.floor(Math.random() * 4);
-				this.AnswersList[correctIndex] = this.country;
-				for (let i = 0; i < 4; i++) {
-					if (i != correctIndex) {
-						let answer = countries[Math.floor(Math.random() * 30)];
-						if (!this.AnswersList.some((i) => i == answer))
-							this.AnswersList[i] = answer;
-						else {
-							while (true) {
-								let answer = countries[Math.floor(Math.random() * 30)];
-								if (!this.AnswersList.some((i) => i == answer)) {
-									this.AnswersList[i] = answer;
-									break;
-								}
-							}
-						}
-					}
-				}
-			},
-			//check Answer if it is true or false
-			checkAnswer(answer) {
-				this.answer = answer;
-				if (this.answer) {
-					if (this.answer == this.country) {
-						return true;
-					} else false;
-				}
-			},
-			AddLeaderBoardUser() {
-				let user = { 'username': this.userName.userName, 'score': this.score.score };
-				axios.post(this.db, user)
-					.then(() => {})
-					.catch(() => {});
-			},
-		},
-		// setVarForGame() {
-		// 	this.isGameFinished = false;
-		// 	this.questionInfo = {
-		// 		country: '',
-		// 		index: 0,
-		// 		img: '',
-		// 		score: '',
-		// 	};
-		// 	this.AnswersList = ['', '', '', ''];
-		// 	this.nextQuestion();
-		// },
-	};
+  methods: {
+    //call This after every choice and check answer
+    choose(ClickedButton) {
+      if (ClickedButton) {
+        let buttons = document.querySelectorAll("button");
+        let btnText = ClickedButton.target.outerText;
+        //disabled buttons till next question
+        buttons.forEach((button) => {
+          button.disabled = true;
+        });
+        //if true make green button and wait for 1 sec
+        if (this.checkAnswer(btnText)) {
+          this.score.score++;
+          ClickedButton.target.classList.add("true");
+          setTimeout(() => {
+            if (this.index < countries.length) {
+              //enable buttons after 3 sec
+              buttons.forEach((button) => {
+                button.classList.remove("true");
+                button.classList.remove("false");
+                button.disabled = false;
+              });
+              this.nextQuestion();
+            }
+          }, 800);
+        }
+        //if not true make red button and wait 2 sec
+        else {
+          ClickedButton.target.classList.add("false");
+          buttons.forEach((button) => {
+            if (button.outerText == this.country) {
+              button.classList.add("true");
+            }
+          });
+          setTimeout(() => {
+            if (this.index <= countries.length) {
+              //enable buttons after 3 sec
+              buttons.forEach((button) => {
+                button.classList.remove("true");
+                button.classList.remove("false");
+                button.disabled = false;
+              });
+              this.nextQuestion();
+            }
+          }, 1000);
+        }
+      }
+      if (this.index == countries.length) {
+        this.isGameFinished.finished = true;
+        this.AddLeaderBoardUser();
+      }
+    },
+    //get next question and increase index of question
+    nextQuestion() {
+      this.getData();
+      this.index++;
+    },
+    // get data from api with axios then set variables for questions
+    getData() {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${countries[this.index]}`)
+        .then((res) => {
+          this.country = res.data[0].name.common;
+          this.img = res.data[0].flags["png"];
+          this.setAnswers();
+        });
+    },
+    //set 1 correct answer from question info and
+    //3 random answers from countries list
+    setAnswers() {
+      let correctIndex = Math.floor(Math.random() * 4);
+      this.AnswersList[correctIndex] = this.country;
+      for (let i = 0; i < 4; i++) {
+        if (i != correctIndex) {
+          let answer = countries[Math.floor(Math.random() * 30)];
+          if (!this.AnswersList.some((i) => i == answer))
+            this.AnswersList[i] = answer;
+          else {
+            while (true) {
+              let answer = countries[Math.floor(Math.random() * 30)];
+              if (!this.AnswersList.some((i) => i == answer)) {
+                this.AnswersList[i] = answer;
+                break;
+              }
+            }
+          }
+        }
+      }
+    },
+    //check Answer if it is true or false
+    checkAnswer(answer) {
+      this.answer = answer;
+      if (this.answer) {
+        if (this.answer == this.country) {
+          return true;
+        } else false;
+      }
+    },
+    AddLeaderBoardUser() {
+      let user = { username: this.userName.userName, score: this.score.score };
+      axios
+        .post(this.db, user)
+        .then(() => {})
+        .catch(() => {});
+    },
+  },
+  // setVarForGame() {
+  // 	this.isGameFinished = false;
+  // 	this.questionInfo = {
+  // 		country: '',
+  // 		index: 0,
+  // 		img: '',
+  // 		score: '',
+  // 	};
+  // 	this.AnswersList = ['', '', '', ''];
+  // 	this.nextQuestion();
+  // },
+};
 </script>
