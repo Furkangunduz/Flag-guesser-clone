04576ff790f2a4c664109b4679b2a9586afc1656
Furkangunduz <frkn.gundz00@gmail.com>
2022-04-13 18:06:29 +0300

    leaderboard

--- src/components/GameComp.vue
+++ src/components/GameComp.vue
@@ -1,55 +1,59 @@
 <template>
-  <start-comp v-if="!isGameStarted" />
-  <question-comp v-if="isGameStarted && !isGameFinished.finished" />
-  <finish-game
-    @restart="reset"
-    v-if="isGameStarted && isGameFinished.finished"
-  />
+    <start-comp v-if="!isGameStarted" />
+    <question-comp v-if="isGameStarted && !isGameFinished.finished" />
+    <finish-game
+        @restart="reset"
+        v-if="isGameStarted && isGameFinished.finished"
+    />
 </template>
 <script>
-import QuestionComp from "@/components/QuestionComp.vue";
-import StartComp from "@/components/StartComp.vue";
-import FinishGame from "./FinishComp.vue";
-import countries from "../../country_names";
+import QuestionComp from '@/components/QuestionComp.vue';
+import StartComp from '@/components/StartComp.vue';
+import FinishGame from './FinishComp.vue';
+import countries from '../../country_names';
 
 export default {
-  components: {
-    QuestionComp,
-    StartComp,
-    FinishGame,
-  },
-  data() {
-    return {
-      isGameStarted: false,
-      resetGame: { reset: false },
-      isGameFinished: { finished: false },
-      userName: { userName: "" },
-      countriesLen: countries.length,
-      score: { score: 0 },
-    };
-  },
-  methods: {
-    startGame(userName) {
-      if (!userName) {
-        alert("please write username");
-        return;
-      }
-      this.userName.userName = userName;
-      this.isGameStarted = true;
+    components: {
+        QuestionComp,
+        StartComp,
+        FinishGame,
     },
-    reset() {
-      this.isGameFinished.finished = false;
-      this.isGameStarted = false;
+    data() {
+        return {
+            isGameStarted: false,
+            resetGame: { reset: false },
+            isGameFinished: { finished: false },
+            userName: { userName: '' },
+            countriesLen: countries.length,
+            score: { score: 0 },
+            leaderBoard: {
+                board: [],
+            },
+        };
     },
-  },
-  provide() {
-    return {
-      startGame: this.startGame,
-      isGameFinished: this.isGameFinished,
-      userName: this.userName,
-      countriesLen: this.countriesLen,
-      score: this.score,
-    };
-  },
+    methods: {
+        startGame(userName) {
+            if (!userName) {
+                alert('please write username');
+                return;
+            }
+            this.userName.userName = userName;
+            this.isGameStarted = true;
+        },
+        reset() {
+            this.isGameFinished.finished = false;
+            this.isGameStarted = false;
+        },
+    },
+    provide() {
+        return {
+            startGame: this.startGame,
+            isGameFinished: this.isGameFinished,
+            userName: this.userName,
+            countriesLen: this.countriesLen,
+            score: this.score,
+            db: this.leaderBoard,
+        };
+    },
 };
 </script>
--- src/components/StartComp.vue
+++ src/components/StartComp.vue
@@ -1,52 +1,40 @@
 <template>
-	<div class="start-page">
-		<div class="left">
-			<input
-				v-model="userName"
-				maxlength="8"
-				class="input-name"
-				type="text"
-				placeholder="name"
-			/>
-			<div class="leaderBoard">
-				<ul>
-					<li v-for="player of leaderBoard" :key="player.id">
-						<span style="color: white; font-size: 1.3rem">{{
-							player.username || 'BigBoss'
-						}}</span>
-						==>
-						<span style="color: white; font-size: 1.3rem">{{
-							player.score
-						}}</span>
-					</li>
-				</ul>
-			</div>
-		</div>
+    <div class="start-page">
+        <div class="left">
+            <input
+                v-model="userName"
+                maxlength="8"
+                class="input-name"
+                type="text"
+                placeholder="name"
+            />
+            <div class="leaderBoard">
+                <ul>
+                    <li v-for="player of db.board" :key="player.id">
+                        <span style="color: white">{{
+                            player.username || 'BigBoss'
+                        }}</span>
+                        :
+                        <span style="color: white; font-size: 1.3rem">{{
+                            player.score
+                        }}</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
-		<button class="basla-btn" @click="startGame(userName)">Basla</button>
-	</div>
+        <button class="basla-btn" @click="startGame(userName)">Basla</button>
+    </div>
 </template>
 <script>
-	const axios = require('axios');
-	export default {
-		data() {
-			return {
-				userName: '',
-				leaderBoard: [],
-				db: 'https://my-json-server.typicode.com/Furkangunduz/flag-guesser-clone/leaderBoard',
-			};
-		},
-		created() {
-			this.getLeaderBoard();
-		},
-		inject: ['startGame'],
-		methods: {
-			getLeaderBoard() {
-				axios.get(this.db).then((res) => {
-					this.leaderBoard = res.data;
-				});
-			},
-		},
-	};
+const axios = require('axios');
+export default {
+    data() {
+        return {
+            userName: '',
+        };
+    },
+    inject: ['startGame', 'db'],
+};
 </script>
 <style scoped></style>
